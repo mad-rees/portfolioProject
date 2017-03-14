bfb54c763c91e597dc3368ae7490e454b46ab59b
Christopher Rees <christopherrees@Christophers-MacBook-Pro.local>
2017-03-14 00:46:35 -0300

    Portfolio default values

--- app/models/site_portfolio.rb
+++ app/models/site_portfolio.rb
@@ -5,5 +5,12 @@ class SitePortfolio < ApplicationRecord
 		where(subtitle: "AngularJS")
 	end
 	scope :ruby_on_rails_portfolio_items, -> { where(subtitle: "Ruby on Rails") }
+	
+	after_initialize :set_defaults
 		
+		
+	def set_defaults
+		self.main_image ||= "http://placehold.it/600x400"
+		self.thumb_image ||=  "http://placehold.it/350x200"
+	end
 end
