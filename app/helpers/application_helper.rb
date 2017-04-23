0e956efcad3450e9bdce017e608f7f1b248ff4de
Christopher Rees <christopherrees@Christophers-MacBook-Pro.local>
2017-04-23 00:09:21 -0300

    Implement collection based partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,6 @@
 module ApplicationHelper
+# 	Defines a helper for the links to login, register and logout intelligently
+#    based on the user's session status
 	def login_helper
 		if current_user.is_a?(User) 
 			link_to "Logout", destroy_user_session_path, method: :delete
@@ -8,6 +10,8 @@ module ApplicationHelper
 			(link_to "Login", new_user_session_path)
         	end
         end
+#   Defines a helper for the session welcome greeting which includes the source
+#   of the user (which site they've come from) and tells them where they are in the application. 
     def source_helper(layout_name)
 	    if session[:source]
 		    greeting = "Thanks for visiting my #{layout_name} page from #{session[:source]}"
