336e52f5838efb3eb4f15b81cae264acc4362000
Christopher Rees <christopherrees@Christophers-MacBook-Pro.local>
2017-04-25 01:50:39 -0300

    Authorization with Petergate installed

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20170320021157) do
-=======
-ActiveRecord::Schema.define(version: 20170406005825) do
->>>>>>> authentication
+ActiveRecord::Schema.define(version: 20170425044832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,8 +70,6 @@ ActiveRecord::Schema.define(version: 20170406005825) do
     t.datetime "updated_at", null: false
   end
 
-<<<<<<< HEAD
-=======
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -94,11 +88,11 @@ ActiveRecord::Schema.define(version: 20170406005825) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "roles"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
->>>>>>> authentication
   add_foreign_key "blogs", "topics"
   add_foreign_key "technologies", "site_portfolios"
 end
